5f662503e25fff9914baeab4b584c466733baa4d
pe5 <eero.perola@helsinki.fi>
2018-11-24 18:06:42 +0200

    lisätty Bootstrap toiminnollisuudet ohjelmalle

--- application/views.py
+++ application/views.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from application import app
+from application.tasks.models import Task
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", needs_answers=Task.find_questions_with_no_answers())
